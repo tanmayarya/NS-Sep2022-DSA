91538ea9a824dd025adbe6f797da08e152d1d210
Tanmay Arya <tanmayarya80@gmail.com>
2022-09-26 22:52:35 +0530

    Intro to programming

---
+++ src/Basics/Intro.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		
+		int a; // declaration
+		
+		a = 20; // initialization
+		System.out.println(a);
+		
+		a = 30;
+		System.out.println(a);
+	}
+
+}
